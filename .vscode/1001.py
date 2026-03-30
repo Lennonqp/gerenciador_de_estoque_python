a1da056b90437b710755d5de79a200a13c1e6d55
Lennonqp <lennonqpscs@gmail.com>
2026-03-30 18:42:53 -0300

    Limpando o repositório e mantendo apenas o código principal

--- .vscode/1001.py
+++
@@ -1,4 +0,0 @@
-primeiro = int(input("Digite o primeiro número: "))
-segundo = int(input("Digite o segundo número: "))
-
-print(f"A soma do númerom {primeiro} com o número {segundo} é de: {primeiro + segundo}")
